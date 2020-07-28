98c7874647f72357fc707bdd71b160120908a101
abirkhan <abir1sumon@gmail.com>
2020-07-28 12:55:42 +0600

    [] Controllers POST, PUT, DELETE operations are secured with JWT tokens.

--- app/routes/article.route.js
+++ app/routes/article.route.js
@@ -1,9 +1,11 @@
+var VerifyToken = require('../auth/verify.token');
+
 module.exports = (app) => {
     const articleController = require('../controllers/article.controller.js');
     app.get('/articles', articleController.get);
     app.get('/article/:uri', articleController.getByUri)
     app.get('/published-article/:uri', articleController.getPublishedArticleByUrl)
-    app.post('/article', articleController.post);
-    app.put('/article', articleController.put);
-    app.delete('/article/:id', articleController.delete);
+    app.post('/article', VerifyToken, articleController.post);
+    app.put('/article', VerifyToken, articleController.put);
+    app.delete('/article/:id', VerifyToken, articleController.delete);
 }
--- app/routes/auth.route.js
+++ app/routes/auth.route.js
@@ -1,4 +1,6 @@
+var VerifyToken = require('../auth/verify.token');
+
 module.exports = (app) => {
     const authController = require("../controllers/auth.controller");
-    app.post('/register', authController.register);
+    app.post('/register', VerifyToken, authController.register);
 }
