83bc58980cce50e4632ed821aa7fb3c8eeaf896c
abirkhan <abir1sumon@gmail.com>
2020-07-30 13:27:15 +0600

    [] Meta description is added.

--- app/model/article.js
+++ app/model/article.js
@@ -6,6 +6,9 @@ module.exports = (sequelize, Sequelize) => {
     title: {
       type: Sequelize.STRING,
     },
+    description: {
+      type: Sequelize.STRING,
+    },
     author: {
       type: Sequelize.STRING,
     },
